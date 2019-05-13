c5631581a3b9613f857a2ed89f3e97b11b2f0747
davidhurguly <45793371+davidhurguly@users.noreply.github.com>
2019-05-13 20:15:52 +0200

    token & activity

---
+++ app/src/main/java/com/szte/firebasepoke/java/MainActivity.java
@@ -0,0 +1,41 @@
+package com.szte.firebasepoke.java;
+
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.szte.firebasepoke.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView tokenDisplay = findViewById(R.id.tokenDisplay);
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        tokenDisplay.setText(task.getResult().getToken());
+                    }
+                });
+    }
+
+}
